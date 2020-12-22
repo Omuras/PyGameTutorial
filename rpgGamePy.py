b6cd7ff9ff3f4b6b570459599ae883e2991fc721
Omuras <omuras123@gmail.com>
2020-12-22 15:24:54 +0300

    Screen Created

--- rpgGamePy.py
+++ rpgGamePy.py
@@ -1,5 +1,16 @@
+#pygame library
 import pygame
 
-def gameScreen():
-#Oyun ekrani
-    
+#Screen size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+#ScreenColor as RGB codes
+WHITE_COLOR = (255,255,255)
+BLACK_COLOR = (0,0,0)
+
+#Create window with specified sizes
+game_screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+#Set window color to white
+game_screen.fill(WHITE_COLOR)
+
+
